188964c435a2e111c718a767fb26f42d1cbcc888
zfc <476409251@qq.com>
2018-03-21 21:57:13 +0800

    1.2

--- gulpfile.js
+++ gulpfile.js
@@ -3,26 +3,13 @@ const imagemin = require('gulp-imagemin');
 const imagemin2 = require('imagemin');
 const imageminJpegtran = require('imagemin-jpegtran');
 const imageminPngquant = require('imagemin-pngquant');
-const cssmin = require('gulp-cssmin');
-const uglify = require('gulp-uglify');
-const pump = require('pump');
-const htmlminify = require("gulp-html-minify");
+const cssmin = require('gulp-cssmin');
+const uglify = require('gulp-uglify');
+const pump = require('pump');
+const htmlminify = require("gulp-html-minify");
 
 
-
-
-gulp.task('default', () =>
-    gulp.src('src/app.css')
-    .pipe(autoprefixer({
-        browsers: ['last 2 versions'],
-        cascade: false
-    }))
-    .pipe(gulp.dest('dist'))
-    );
-
-
-
-gulp.task('default',['imgmin','htmlmin','cssmin','jsmin','video'], () => {
+gulp.task('default', ['imgmin', 'htmlmin', 'cssmin', 'jsmin', 'video'], () = > {
     // gulp.src(['./src/**/*.{jpg,png}'])
     //     .pipe(imagemin())
     //     .pipe(gulp.dest('./dist'))
@@ -36,23 +23,25 @@ gulp.task('default',['imgmin','htmlmin','cssmin','jsmin','video'], () => {
     // then(files => {
     // 	console.log(files);    
     // })
-});
+}
+)
+;
 
 
-gulp.task('htmlmin' , function(){
-        return gulp.src("./src/**/*.html")
-            .pipe(htmlminify())
-            .pipe(gulp.dest("./build"))
+gulp.task('htmlmin', function () {
+    return gulp.src("./src/**/*.html")
+        .pipe(htmlminify())
+        .pipe(gulp.dest("./build"))
 });
 
 // ok
-gulp.task('cssmin',()=>{
+gulp.task('cssmin', () = > {
     gulp.src('./src/**/*.css')
     .pipe(cssmin())
-        .pipe(gulp.dest('./build'));
+    .pipe(gulp.dest('./build'));
 })
 
-gulp.task('test-css-prefixer',()=>{
+gulp.task('test-css-prefixer', () = > {
     gulp.src('./test/*.css')
     .pipe(autoprefixer({
         browsers: ['>1%']
@@ -61,26 +50,26 @@ gulp.task('test-css-prefixer',()=>{
 })
 
 
-gulp.task('jsmin', function (cb) {
-      pump([
-                gulp.src('./src/**/*.js'),
-                uglify(),
-                gulp.dest('./build')
-            ],
-          );
+gulp.task('jsmin', function (cb) {
+    pump([
+            gulp.src('./src/**/*.js'),
+            uglify(),
+            gulp.dest('./build')
+        ],
+    );
 });
 
 
-    // ok
-    gulp.task('imgmin',()=>{
-        gulp.src(['./src/**/*.{jpg,png,jpeg}'])
-        .pipe(imagemin())
-        .pipe(gulp.dest('./build'))
-    })
+// ok
+gulp.task('imgmin', () = > {
+    gulp.src(['./src/**/*.{jpg,png,jpeg}'])
+    .pipe(imagemin())
+    .pipe(gulp.dest('./build'))
+})
 
-    gulp.task('video',()=>{
-        gulp.src(['./src/video/*.mp4'])
-        .pipe(gulp.dest('./build/video'))
-    })
+gulp.task('video', () = > {
+    gulp.src(['./src/video/*.mp4'])
+    .pipe(gulp.dest('./build/video'))
+})
 
 
